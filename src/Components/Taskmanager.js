d25c8b1b7183ea1db40cceb81b357eecf7c61440
nidhinsaju <smurfy7879@gmail.com>
2025-03-04 15:52:16 +0530

    updates

--- src/Components/Taskmanager.js
+++ src/Components/Taskmanager.js
@@ -1,32 +1,30 @@
-import React, { useState, useRef, useEffect } from 'react';
-import './Taskmanager.css';
+import React, { useState, useRef, useEffect } from "react";
+import "./Taskmanager.css";
 import { IoMdDoneAll } from "react-icons/io";
 import { FiEdit } from "react-icons/fi";
 import { MdDelete } from "react-icons/md";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Taskmanager() {
-  const [todo, setTodo] = useState('');
+  const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
   const [editId, setEditID] = useState(null);
-  const [filter, setFilter] = useState('All');
+  const [filter, setFilter] = useState("All");
   const inputRef = useRef(null);
 
- 
   useEffect(() => {
-    const storedTodos = localStorage.getItem('todos');
+    const storedTodos = localStorage.getItem("todos");
     if (storedTodos) {
       setTodos(JSON.parse(storedTodos));
     }
     inputRef.current.focus();
   }, []);
 
-  
   useEffect(() => {
     if (todos.length > 0) {
-      localStorage.setItem('todos', JSON.stringify(todos));
+      localStorage.setItem("todos", JSON.stringify(todos));
     } else {
-      localStorage.removeItem('todos'); 
+      localStorage.removeItem("todos");
     }
   }, [todos]);
 
@@ -43,11 +41,14 @@ function Taskmanager() {
       setTodos(updatedTodos);
       setEditID(null);
     } else {
-      if (todo.trim() !== '') {
-        setTodos([...todos, { list: todo, id: Date.now(), status: 'In Progress' }]);
+      if (todo.trim() !== "") {
+        setTodos([
+          ...todos,
+          { list: todo, id: Date.now(), status: "In Progress" },
+        ]);
       }
     }
-    setTodo('');
+    setTodo("");
   };
 
   const onDelete = (id) => {
@@ -57,7 +58,12 @@ function Taskmanager() {
   const onComplete = (id) => {
     setTodos(
       todos.map((to) =>
-        to.id === id ? { ...to, status: to.status === 'Complete' ? 'In Progress' : 'Complete' } : to
+        to.id === id
+          ? {
+              ...to,
+              status: to.status === "Complete" ? "In Progress" : "Complete",
+            }
+          : to
       )
     );
   };
@@ -71,68 +77,76 @@ function Taskmanager() {
   };
 
   const filteredTodos = todos.filter((to) => {
-    if (filter === 'All') return true;
+    if (filter === "All") return true;
     return to.status === filter;
   });
 
+ /*  const current = new Date();
+  const date = `${current.getDate()}/${
+    current.getMonth() + 1
+  }/${current.getFullYear()}`; */
+
   return (
-
     <div>
-     <Link to="Api-demo">
-  <input type="button" value="API DEMO" className="api-demo-btn" />
-</Link>
-    <div className='container'>
-      <h2>Task Manager App</h2>
-      <form className='form-group' onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={todo}
-          ref={inputRef}
-          placeholder='Enter your Task'
-          className='form-control1'
-          onChange={(event) => setTodo(event.target.value)}
-        />
-        <button type="submit">{editId ? 'EDIT' : 'ADD'}</button>
-      </form>
+      {/* <Link to="Api-demo">
+        <input type="button" value="API DEMO" className="api-demo-btn" />
+      </Link> */}
+      <div className="container">
+        <h2>Task Manager App {/* {date} */}</h2>
 
-      <div className="filter-buttons">
-        <button onClick={() => setFilter('All')}>All</button>
-        <button onClick={() => setFilter('In Progress')}>In Progress</button>
-        <button onClick={() => setFilter('Complete')}>Complete</button>
-      </div>
+        <form className="form-group" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={todo}
+            ref={inputRef}
+            placeholder="Enter your Task"
+            className="form-control1"
+            onChange={(event) => setTodo(event.target.value)}
+          />
+          <button type="submit">{editId ? "EDIT" : "ADD"}</button>
+        </form>
 
-      <div className='list'>
-        <ul>
-          {filteredTodos.map((to) => (
-            <li key={to.id} className="list-items">
-              <div className="list-item-list" id={to.status === 'Complete' ? 'list-item' : ''}>
-                {to.list}
-              </div>
-              <span>
-                <IoMdDoneAll
-                  className="list-item-icons"
-                  id='complete'
-                  title="Complete"
-                  onClick={() => onComplete(to.id)}
-                />
-                <FiEdit
-                  className="list-item-icons"
-                  id='edit'
-                  title="Edit"
-                  onClick={() => onEdit(to.id)}
-                />
-                <MdDelete
-                  className="list-item-icons"
-                  id='delete'
-                  title="Delete"
-                  onClick={() => onDelete(to.id)}
-                />
-              </span>
-            </li>
-          ))}
-        </ul>
+        <div className="filter-buttons">
+          <button onClick={() => setFilter("All")}>All</button>
+          <button onClick={() => setFilter("In Progress")}>In Progress</button>
+          <button onClick={() => setFilter("Complete")}>Complete</button>
+        </div>
+
+        <div className="list">
+          <ul>
+            {filteredTodos.map((to) => (
+              <li key={to.id} className="list-items">
+                <div
+                  className="list-item-list"
+                  id={to.status === "Complete" ? "list-item" : ""}
+                >
+                  {to.list}
+                </div>
+                <span>
+                  <IoMdDoneAll
+                    className="list-item-icons"
+                    id="complete"
+                    title="Complete"
+                    onClick={() => onComplete(to.id)}
+                  />
+                  <FiEdit
+                    className="list-item-icons"
+                    id="edit"
+                    title="Edit"
+                    onClick={() => onEdit(to.id)}
+                  />
+                  <MdDelete
+                    className="list-item-icons"
+                    id="delete"
+                    title="Delete"
+                    onClick={() => onDelete(to.id)}
+                  />
+                </span>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
